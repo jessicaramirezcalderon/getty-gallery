bf9308e3ac216d5c5f9435db97a64c8c9eb6b7d7
Ivan Orellana Jr <iorellana21@gmail.com>
2021-01-25 23:22:05 -0500

    adding for-loop to generate 10 pics

--- api.js
+++ api.js
@@ -1,13 +1,12 @@
 // ivan start
-// create function that states what occurs when a city button is clicked
+// create function that states pulls id of city button, calls API to gather images and append to carousel
 function displayImg() {
   // 1. pull ID of city to use in queryURL - need variable to contain ID - this is determine when city button is clicked
   var cityName = $(this).attr("id");
   console.log(cityName);
 
   // 2. update queryURL; inserting city name via variable
-  var queryURL = "https://api.unsplash.com/search/collections?page=1&query=" + cityName + "&client_id=pFIWUHqX-OP7nX7XPXSeK2lpEddTVseWjHAQ0ct9mQI";
-
+  var queryURL = "https://api.unsplash.com/photos/random?query=" + cityName +"&count=10&client_id=pFIWUHqX-OP7nX7XPXSeK2lpEddTVseWjHAQ0ct9mQI";
   // 3. call ajax method to send a request
   $.ajax({
     url: queryURL,
@@ -20,23 +19,26 @@ function displayImg() {
       // Log the resulting object
       console.log(response);
 
-      // 5. create div tag that will contain img and append carousel-item classes
-      var divImg = $("<div>").addClass("carousel-item");
+      // 5. for-loop to create tags for images and append to carousel
+      for(var i = 0; i < 10; i++){
+        // 6. create div tag that will contain img and append carousel-item classes
+        var divImg = $("<div>").addClass("carousel-item");
 
-      // 6. create img tag that will contain city img and append d-block w-100 classes
-      var cityImg = $("<img>").addClass("d-block w-100");
-      // add src and alt attributes - THIS IS WHERE THE MAGIC NEEDS TO HAPPEN
-      console.log(response.results[0]);
-      console.log(response.results[0].title);
-      console.log(response.results[0].cover_photo.urls.regular);
+        // 7. create img tag that will contain city img and append d-block w-100 classes
+        var cityImg = $("<img>").addClass("d-block w-100");
+        // add src and alt attributes - THIS IS WHERE THE MAGIC NEEDS TO HAPPEN
+        console.log(response[i]);
+        console.log(response[i].urls.regular);
+        console.log(response[i].location.city);
 
-      $(cityImg).attr("src", `${response.results[0].cover_photo.urls.regular}`);
-      $(cityImg).attr("alt", `${response.results[0].title}`);
-      // append cityImg to divImg
-      $(divImg).append(cityImg);
+        $(cityImg).attr("src", `${response[i].urls.regular}`);
+        $(cityImg).attr("alt", `${response[i].location.city}`);
+        // append cityImg to divImg
+        $(divImg).append(cityImg);
 
-      // 7. append divImg to carousel-inner class
-      $(".carousel-inner").append(divImg);
+        // 8. append divImg to carousel-inner class
+        $(".carousel-inner").append(divImg);
+      }
     });
 }
 
