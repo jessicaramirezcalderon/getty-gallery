d67ce5f91845fe18c3f9d10f4567b36ac49fffe9
Jessi RC <jessy.ramirez23@gmail.com>
2021-02-03 15:34:44 -0500

    solve merge conflict

--- api.js
+++ api.js
@@ -3,15 +3,13 @@ let alreadyLoaded = false;
 
 //Foundation search bar
 $(document).foundation();
-$('.search')
-  .on('click', function (event) {
-    $(".search-field").toggleClass("expand-search");
-
-    // if the search field is expanded, focus on it
-    if ($(".search-field").hasClass("expand-search")) {
-      $(".search-field").focus();
-    }
-  })
+$('.search').on('click', function (event) {
+  $(".search-field").toggleClass("expand-search");
+  // if the search field is expanded, focus on it
+  if ($(".search-field").hasClass("expand-search")) {
+    $(".search-field").focus();
+  }
+});
 
 //grab the input data before using it
 const cityInput = $("#city-input");
@@ -27,14 +25,15 @@ function displayImg(e) {
   // store the input info from line 23 as value to be passed in function below
   const cityName = cityInput.val();
 
-  // API Query call
-  const queryURL = "https://api.unsplash.com/photos/random?query=breakfast+" + cityName + "&count=10&orientation=landscape&client_id=V3iL4jHxM2p-IjPs13Jjt_v8QdU7RsoO6ZyZQjymdSU";
+  var apiKey = "c7cde66d595fb98577da25bd96a3df85";
+  // query URL for city weather
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
 
+  // API call for city weather
   $.ajax({
     url: queryURL,
     method: "GET"
   })
-    // Promise function
     .then(function (response) {
       //Insert onto the elements defined in loop below the images once the first load occurs
       if (alreadyLoaded) {
@@ -47,40 +46,39 @@ function displayImg(e) {
 
       }
 
-      else {
-        //for-loop to create tags for images and append to carousel
-        for (var i = 0; i < 10; i++) {
-          //create div tag that will contain img and append carousel-item classes
-          var divImg = $("<div>").addClass("carousel-item");
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // Promise function
+    .then(function (response) {
+      console.log("unsplash url - " + queryURL);
 
-          //create img tag that will contain city img and append d-block w-100 classes
-          var cityImg = $("<img>").addClass("d-block w-100");
+      // for-loop to create tags for images and append to carousel
+      for (var i = 0; i < response.length; i++) {
+        //create div tag that will contain img and append carousel-item classes
+        var divImg = $("<div>").addClass("carousel-item");
+        divImg.attr("id", i);
 
-          $(cityImg).attr("src", `${response[i].urls.regular}`);
-          $(cityImg).attr("alt", `${response[i].location.city}`);
-          // append cityImg to divImg
-          $(divImg).append(cityImg);
+        //create img tag that will contain city img and append d-block w-100 classes
+        var cityImg = $("<img>").addClass("d-block w-100");
+        $(cityImg).attr("src", `${response[i].urls.regular}`);
+        $(cityImg).attr("alt", `${response[i].location.city}`);
 
-          // append divImg to carousel-inner class
-          $(".carousel-inner").append(divImg);
+        // append cityImg to divImg
+        $(divImg).append(cityImg);
+        // append divImg to carousel-inner class
+        $(".carousel-inner").append(divImg);
 
-          alreadyLoaded = true;
-        }
+        var firstImgTag = $("#0").addClass("active");
       }
 
     });
 }
 
-// playing with event listener when a city button is clicked to display pics for that city in carousel - calling displayImg function
-//Removed the on click event since we're now asking people to search instead of clicking a button
-//$("button").on("click", displayImg);
+// user clicks Enter, function kicks off to get current weather and then city images
+cityInput.on("keypress", currentWeatherAPI);
 
-cityInput.on("keypress", displayImg);
-
-
-/*Appended the images and corresponding attributes to the carousel using a for loop
-Ivan
-*/
 
 /*Added the functionality to load different images for each city. Added if and else statements for each state
 Commented out some console logs
